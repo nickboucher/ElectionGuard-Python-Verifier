ce73e6bbc9814a33e2c32fbc23a5784a7a5e1f3d
Nicholas Boucher <nicholas.d.boucher@gmail.com>
2020-02-10 01:15:13 -0800

    Command line argument parser

--- verify.py
+++ verify.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-
 # ElectionGuard Verifier
 # Nicholas Boucher 2020
+import argparse
 
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(
+            description='Verify ElectionGuard Elections Results.')
+    parser.add_argument('election-data',
+                        help='ElectionGuard results JSON file.')
+    args = parser.parse_args()
+    print(args)
 
 
 if __name__ == '__main__':
