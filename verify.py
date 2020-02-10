6fc22085383ef78ea42b94195ecabe9fcf9c56eb
Nicholas Boucher <nicholas.d.boucher@gmail.com>
2020-02-10 01:37:53 -0800

    Read election results JSON

--- verify.py
+++ verify.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 # ElectionGuard Verifier
 # Nicholas Boucher 2020
-import argparse
+from argparse import ArgumentParser
+from json import load
+from os.path import isfile
+from sys import exit
 
 
-def main():
-    parser = argparse.ArgumentParser(
+def main() -> None:
+    # Parse argument from command line
+    parser = ArgumentParser(
             description='Verify ElectionGuard Elections Results.')
-    parser.add_argument('election-data',
+    parser.add_argument('election_data',
                         help='ElectionGuard results JSON file.')
     args = parser.parse_args()
-    print(args)
+    if not isfile(args.election_data):
+        exit("election_data is invalid file path")
+
+    # Read election results
+    with open(args.election_data, 'r') as f:
+        election_data = load(f)
+    print(election_data)
 
 
 if __name__ == '__main__':
