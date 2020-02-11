4b562a747f8fbc2b7b53e4815e2f530f25952c2e
Nicholas Boucher <nicholas.d.boucher@gmail.com>
2020-02-10 22:32:23 -0800

    Trustee threshold checks

--- verify.py
+++ verify.py
@@ -36,7 +36,7 @@ def main() -> None:
 
 def verify_json(election_data: Dict, log: Logger = None) -> bool:
     """Returns true when the deserialized json `election_data` is valid.
-        Optionally, info can be logged by passing a Logger."""
+       Optionally, info can be logged by passing a Logger."""
     # Setup logging infrastructure
     if log is None:
         log = getLogger("verify_json")
@@ -53,7 +53,7 @@ def verify_json(election_data: Dict, log: Logger = None) -> bool:
 
 def verify_election(election_data: Dict, log: Logger = None) -> bool:
     """Returns true when the election results `election_data` are valid.
-        Optionally, info can be logged by passing a Logger."""
+       Optionally, info can be logged by passing a Logger."""
     # Setup logging infrastructure
     if log is None:
         log = getLogger("verify_json")
@@ -63,17 +63,41 @@ def verify_election(election_data: Dict, log: Logger = None) -> bool:
     if not verify_json(election_data, log):
         return False
 
-    # Check that the number of trustees is greater than zero
+    def fail(message: str) -> bool:
+        """Outputs a failure log message with the passed text and
+           returns false."""
+        log.warning(message)
+        return False
+
+    # Test: The number of trustees who can together decrypt the election is
+    # greater than zero
     try:
         num_trustees = int(election_data['parameters']['num_trustees'])
     except ValueError:
-        log.warning(f"'num_trustees' value "
+        return fail(f"'num_trustees' value "
                     "'{election_data['parameters']['num_trustees']}' is not a "
                     "valid number.")
-        return False
     if num_trustees <= 0:
-        log.warning(f"Invalid number of trustees: {num_trustees}")
-        return False
+        return fail(f"Invalid number of trustees: {num_trustees}")
+
+    # Test: The threshold of trustees necessary to decrypt the election is
+    # greater than zero
+    try:
+        trustees_threshold = int(election_data['parameters']['threshold'])
+    except ValueError:
+        return fail("'threshold' value "
+                    f"'{election_data['parameters']['threshold']}' is not a "
+                    "valid number.")
+    if trustees_threshold <= 0:
+        return fail("Invalid trustee decryption threshold: "
+                    f"{trustees_threshold}")
+
+    # Test: The threshold of trustees necessary to decrypt the election is not
+    # greater than the total number of trustees
+    if trustees_threshold > num_trustees:
+        return fail(f"Trustee decryption threshold ({trustees_threshold}) is "
+                    "greater than the total number of trustees "
+                    f"({num_trustees}).")
 
 
 if __name__ == '__main__':
