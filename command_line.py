5b2fc807c0f1b9630189de411438dcf7fc6da867
Nicholas Boucher <nicholas.d.boucher@gmail.com>
2020-02-23 17:07:35 -0800

    Extended Base Hash Calculation

--- command_line.py
+++ command_line.py
@@ -1,28 +1,57 @@
 #!/usr/bin/env python3
 # ElectionGuard Command Line Verifier Utility
 # Nicholas Boucher 2020
-from argparse import ArgumentParser
-from logging import getLogger, WARNING
+from argparse import ArgumentParser, Namespace
+from os.path import isfile
+from logging import basicConfig, getLogger
 from verify import verify_election
 from models import Record
+from constants import LOG_VERBOSE, LOG_NORMAL
 
 
 def main() -> None:
     """Main point of entry for command line execution."""
     # Parse argument from command line
+    args = parse_args()
+
     # Setup logging infrastructure
-    log = getLogger()
-    log.setLevel(WARNING)
+    basicConfig(format='%(message)s')
+    log = getLogger("election_verifier")
+    if args.verbose:
+        log.setLevel(LOG_VERBOSE)
+    else:
+        log.setLevel(LOG_NORMAL)
 
     # Deserialize election data JSON
-    with open("env/valid_encrypted.json", 'r') as f:
+    with open(args.election_data, 'r') as f:
         election = Record.deserialize(f.read())
 
     # Verify the election results
     if verify_election(election):
-        log.warning("Election Valid.")
+        log.log(LOG_NORMAL, "Election Valid.")
     else:
-        log.warning("Election Invalid.")
+        log.log(LOG_NORMAL, "Election Invalid.")
+
+
+def parse_args() -> Namespace:
+    """Parses, verifies, and returns command line line arguments."""
+    # Configure arguments
+    parser = ArgumentParser(
+            description='Verify ElectionGuard Elections Results.')
+    parser.add_argument('election_data',
+                        help='ElectionGuard results JSON file.')
+    parser.add_argument('--verbose', '-v', action='store_true',
+                        help="Verbose output.")
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Verify arguments
+    if not isfile(args.election_data):
+        exit("election_data is invalid file path")
+    
+    # Return results
+    return args
 
 
 if __name__ == '__main__':
--- verify.py
+++ verify.py
@@ -1,23 +1,14 @@
 #!/usr/bin/env python3
 # ElectionGuard Verifier
 # Nicholas Boucher 2020
-from logging import Logger, getLogger, CRITICAL
+from logging import basicConfig, getLogger
 from models import Record
+from constants import LOG_NORMAL, LOG_VERBOSE, PRIME, GENERATOR, BYTE_ORDER
+from hashlib import sha256
 
 
-def verify_election(election: Record, log: Logger = None) -> bool:
-    """Returns true when the election results `election_data` are valid.
-       Optionally, info can be logged by passing a Logger."""
-    # Setup logging infrastructure
-    if log is None:
-        log = getLogger("verify_election")
-        log.setLevel(CRITICAL)
-
-    def fail(message: str) -> bool:
-        """Outputs a failure log message with the passed text and
-           returns false."""
-        log.warning(message)
-        return False
+def verify_election(election: Record) -> bool:
+    """Returns true when the election results `election_data` are valid."""
 
     # Test: The number of trustees who can together decrypt the election is
     # greater than zero
@@ -39,4 +30,48 @@ def verify_election(election: Record, log: Logger = None) -> bool:
                     "total number of trustees "
                     f"({election.parameters.num_trustees}).")
 
+    # Test: The encryption parameters of the election (prime modulus)
+    # are valid
+    if election.parameters.prime == PRIME:
+        return fail("Prime modulus is not supported:\n"
+                    f"{election.parameters.prime}")
+    
+    # Test: The encryption parameters of the election (group generator)
+    # are valid
+    if election.parameters.generator == GENERATOR:
+        return fail("Group generator is not valid:\n"
+                    f"{election.parameters.generator}")
+
+    # Test: The "extended base hash" was computed correctly
+    extended_base_hash = sha256()
+    for public_key in election.trustee_public_keys:
+        for coefficient in public_key.coefficients:
+            extended_base_hash.update(int_to_bytes(coefficient.public_key))
+    extended_base_hash.update(int_to_bytes(election.base_hash))
+    extended_base_hash = int.from_bytes(extended_base_hash.digest(), BYTE_ORDER)
+    if election.extended_base_hash != extended_base_hash:
+        return fail("Extended base hash is not valid:\n"
+                    f"{election.extended_base_hash}")
+
     return True
+
+def int_to_bytes(num: int) -> bytes:
+    num_bytes = 0
+    temp = num
+    while temp != 0:
+        temp >>= 8
+        num_bytes += 1
+    return num.to_bytes(num_bytes, BYTE_ORDER)
+
+def fail(message: str) -> bool:
+    """Outputs a failure log message with the passed text and
+        returns false."""
+    # Get logging infrastructure
+    basicConfig(format='%(message)s')
+    log = getLogger("election_verifier")
+
+    # Log failure in verbose mode
+    log.log(LOG_VERBOSE, f"{message}\n")
+
+    # Return false for failure
+    return False
